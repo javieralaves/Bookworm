5295fcf770cebd0e9baa2020378331314d0e4cb8
Javier Alaves <jayalaves@gmail.com>
2023-07-22 15:02:46 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Javier Alaves on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
